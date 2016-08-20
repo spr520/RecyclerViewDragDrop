d8d6041e8532d39d2217bbd32b54ec3eedf442ea
Spr <spr520@gmail.com>
2016-08-21 01:19:44 +0800

    grid layout(1)

--- app/src/main/java/com/spr/ninegridrecyclerview/MainActivity.java
+++ app/src/main/java/com/spr/ninegridrecyclerview/MainActivity.java
@@ -1,20 +1,62 @@
 package com.spr.ninegridrecyclerview;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView moveCounter;
+    private TextView feedbackText;
+    private Button[] buttons;
+    private Boolean bad_move = false;
+    private static final Integer[] goal = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+    private ArrayList<Integer> cells = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttons = findButtons();
+
+        for (int i = 0; i < 9; i++) {
+            this.cells.add(i);
+        }
+        Collections.shuffle(this.cells); //random cells array
+
+        fill_grid();
+
+
+        moveCounter = (TextView) findViewById(R.id.MoveCounter);
+        feedbackText = (TextView) findViewById(R.id.FeedbackText);
+
+        for (int i = 1; i < 9; i++) {
+            /*
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    makeMove((Button) v);
+                }
+            });
+            */
+
+            buttons[i].setOnTouchListener(new MyTouchListener());
+        }
+
+
+        moveCounter.setText("0");
+        feedbackText.setText(R.string.game_feedback_text);
+        /*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
@@ -48,5 +91,293 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private int _xDelta;
+    private int _yDelta;
+
+    private final class MyTouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    FrameLayout.LayoutParams lParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    Log.d("Spr","ACTION_DOWN");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.d("Spr","ACTION_UP");
+                    makeMove((Button) view);
+                    if(bad_move) {
+                        // reset to org position
+
+                    }
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    Log.d("Spr","ACTION_MOVE X =" + X);
+                    if(!isAllowMove((Button) view)) {
+                        return true;
+                    }
+
+                    FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+                    int move_x = X - _xDelta;
+                    int move_y = Y - _yDelta;
+                    layoutParams.leftMargin = X - _xDelta;
+                    layoutParams.topMargin = Y - _yDelta;
+//                    layoutParams.rightMargin = -250;
+//                    layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            return true;
+        }
+    }
+    /*
+        public boolean onTouch(View view, MotionEvent event) {
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    layoutParams.leftMargin = X - _xDelta;
+                    layoutParams.topMargin = Y - _yDelta;
+                    layoutParams.rightMargin = -250;
+                    layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            return true;
+        }
+    */
+    public Button[] findButtons() {
+        Button[] b = new Button[9];
+
+        b[0] = (Button) findViewById(R.id.Button00);
+        b[1] = (Button) findViewById(R.id.Button01);
+        b[2] = (Button) findViewById(R.id.Button02);
+        b[3] = (Button) findViewById(R.id.Button03);
+        b[4] = (Button) findViewById(R.id.Button04);
+        b[5] = (Button) findViewById(R.id.Button05);
+        b[6] = (Button) findViewById(R.id.Button06);
+        b[7] = (Button) findViewById(R.id.Button07);
+        b[8] = (Button) findViewById(R.id.Button08);
+        return b;
+    }
+
+    public boolean isAllowMove(final Button b) {
+        int b_text, b_pos, zuk_pos;
+        boolean isAllowMove = false;
+        b_text = Integer.parseInt((String) b.getText());
+        b_pos = find_pos(b_text);
+        zuk_pos = find_pos(0);
+        switch (zuk_pos) {
+            case (0):
+                if (b_pos == 1 || b_pos == 3)
+                    isAllowMove = true;
+                break;
+            case (1):
+                if (b_pos == 0 || b_pos == 2 || b_pos == 4)
+                    isAllowMove = true;
+                break;
+            case (2):
+                if (b_pos == 1 || b_pos == 5)
+                    isAllowMove = true;
+                break;
+            case (3):
+                if (b_pos == 0 || b_pos == 4 || b_pos == 6)
+                    isAllowMove = true;
+                break;
+            case (4):
+                if (b_pos == 1 || b_pos == 3 || b_pos == 5 || b_pos == 7)
+                    isAllowMove = true;
+                break;
+            case (5):
+                if (b_pos == 2 || b_pos == 4 || b_pos == 8)
+                    isAllowMove = true;
+                break;
+            case (6):
+                if (b_pos == 3 || b_pos == 7)
+                    isAllowMove = true;
+                break;
+            case (7):
+                if (b_pos == 4 || b_pos == 6 || b_pos == 8)
+                    isAllowMove = true;
+                break;
+            case (8):
+                if (b_pos == 5 || b_pos == 7)
+                    isAllowMove = true;
+                break;
+        }
+        return isAllowMove;
+    }
+
+    public void makeMove(final Button b) {
+        bad_move = true;
+        int b_text, b_pos, zuk_pos;
+        b_text = Integer.parseInt((String) b.getText());
+        b_pos = find_pos(b_text);
+        zuk_pos = find_pos(0);
+        switch (zuk_pos) {
+            case (0):
+                if (b_pos == 1 || b_pos == 3)
+                    bad_move = false;
+                break;
+            case (1):
+                if (b_pos == 0 || b_pos == 2 || b_pos == 4)
+                    bad_move = false;
+                break;
+            case (2):
+                if (b_pos == 1 || b_pos == 5)
+                    bad_move = false;
+                break;
+            case (3):
+                if (b_pos == 0 || b_pos == 4 || b_pos == 6)
+                    bad_move = false;
+                break;
+            case (4):
+                if (b_pos == 1 || b_pos == 3 || b_pos == 5 || b_pos == 7)
+                    bad_move = false;
+                break;
+            case (5):
+                if (b_pos == 2 || b_pos == 4 || b_pos == 8)
+                    bad_move = false;
+                break;
+            case (6):
+                if (b_pos == 3 || b_pos == 7)
+                    bad_move = false;
+                break;
+            case (7):
+                if (b_pos == 4 || b_pos == 6 || b_pos == 8)
+                    bad_move = false;
+                break;
+            case (8):
+                if (b_pos == 5 || b_pos == 7)
+                    bad_move = false;
+                break;
+        }
+
+        if (bad_move == true) {
+            feedbackText.setText("Move Not Allowed");
+            return;
+        } else {
+            feedbackText.setText("Move OK");
+            cells.remove(b_pos);
+            cells.add(b_pos, 0);
+            cells.remove(zuk_pos);
+            cells.add(zuk_pos, b_text);
+        }
+
+
+        fill_grid();
+        moveCounter.setText(Integer.toString(Integer.parseInt((String) moveCounter.getText()) + 1));
+
+        for (int i = 0; i < 9; i++) {
+            if (cells.get(i) != goal[i]) {
+                return;
+            }
+        }
+        feedbackText.setText("we have a winner");
+    }
+
+    public void fill_grid() {
+        int gridPaddingLeft = getResources().getDimensionPixelSize(R.dimen.grid_layout_padding_left);
+        int gridPaddingTop = getResources().getDimensionPixelSize(R.dimen.grid_layout_padding_top);
+        int cellSize = getResources().getDimensionPixelSize(R.dimen.grid_size);
+        for (int i = 0; i < 9; i++) {
+            int text = cells.get(i);
+            FrameLayout.LayoutParams absParams =
+                    (FrameLayout.LayoutParams) buttons[text].getLayoutParams();
+            switch (i) {
+                case (0):
+
+                    absParams.leftMargin = gridPaddingLeft;
+                    absParams.topMargin = gridPaddingTop;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (1):
+
+                    absParams.leftMargin = gridPaddingLeft + cellSize;
+                    absParams.topMargin = gridPaddingTop;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (2):
+
+                    absParams.leftMargin = gridPaddingLeft + cellSize * 2;
+                    absParams.topMargin = gridPaddingTop;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (3):
+
+                    absParams.leftMargin = gridPaddingLeft;
+                    absParams.topMargin = gridPaddingTop + cellSize;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (4):
+
+                    absParams.leftMargin = gridPaddingLeft + cellSize;
+                    absParams.topMargin = gridPaddingTop + cellSize;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (5):
+
+                    absParams.leftMargin = gridPaddingLeft + cellSize * 2;
+                    absParams.topMargin = gridPaddingTop + cellSize;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (6):
+
+                    absParams.leftMargin = gridPaddingLeft;
+                    absParams.topMargin = gridPaddingTop + cellSize * 2;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (7):
+
+                    absParams.leftMargin = gridPaddingLeft + cellSize;
+                    absParams.topMargin = gridPaddingTop  + cellSize * 2;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (8):
+
+                    absParams.leftMargin = gridPaddingLeft + cellSize * 2;
+                    absParams.topMargin = gridPaddingTop + cellSize * 2;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+
+
+            }
+
+
+        }
+
+    }
+
+    public int find_pos(int element) {
+        int i = 0;
+        for (i = 0; i < 9; i++) {
+            if (cells.get(i) == element) {
+                break;
+            }
+        }
+        return i;
     }
 }
